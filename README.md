d76b20083d04a910f47353175a12ec66aeace7eb
Vighnesh Dheenadhayalan <vd2058@nyu.edu>
2024-04-17 19:34:15 -0400

    First try

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 In this assignment you will create a web app that relies upon a MongoDB database.
 
 Replace the contents of this file with a description of your own web app, as described in [the instructions](./instructions.md).
+dsn="https://92c4adfd7a98e3703a1fbbd709d30230@o4507057354702848.ingest.us.sentry.io/4507057365712896"
--- app.py
+++ app.py
@@ -4,14 +4,12 @@ import os
 import sys
 import subprocess
 import datetime
+#vi .env
 
 from flask import Flask, render_template, request, redirect, url_for, make_response
 
 # import logging
 import sentry_sdk
-from sentry_sdk.integrations.flask import (
-    FlaskIntegration,
-)  # delete this if not using sentry.io
 
 # from markupsafe import escape
 import pymongo
@@ -22,22 +20,6 @@ from dotenv import load_dotenv
 # load credentials and configuration options from .env file
 # if you do not yet have a file named .env, make one based on the template in env.example
 load_dotenv(override=True)  # take environment variables from .env.
-
-# initialize Sentry for help debugging... this requires an account on sentrio.io
-# you will need to set the SENTRY_DSN environment variable to the value provided by Sentry
-# delete this if not using sentry.io
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    # enable_tracing=True,
-    # Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100% of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-    integrations=[FlaskIntegration()],
-    traces_sample_rate=1.0,
-    send_default_pii=True,
-)
 
 # instantiate the app using sentry for debugging
 app = Flask(__name__)
@@ -57,13 +39,10 @@ except ConnectionFailure as e:
     # catch any database errors
     # the ping command failed, so the connection is not available.
     print(" * MongoDB connection error:", e)  # debug
-    sentry_sdk.capture_exception(e)  # send the error to sentry.io. delete if not using
     sys.exit(1)  # this is a catastrophic error, so no reason to continue to live
 
 
 # set up the routes
-
-
 @app.route("/")
 def home():
     """
@@ -72,6 +51,37 @@ def home():
     """
     return render_template("index.html")
 
+@app.route("/newaccount")
+def newaccount():
+    """
+    Route for GET requests to the new_account creation page.
+    Displays a form users can fill out to create a new account.
+    """
+    return render_template("newaccount.html")
+
+@app.route("/addmoney")
+def addmoney():
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged transaction
+    """
+    return render_template("addmoney.html")
+
+@app.route("/create")
+def create():
+    """
+    Route for GET requests to the create page.
+    Displays a form users can fill out to create a new document.
+    """
+    return render_template("create.html")  # render the create template
+
+@app.route("/updatewindow")
+def updatewindow():
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged transaction
+    """
+    return render_template("updatewindow.html")
 
 @app.route("/read")
 def read():
@@ -84,33 +94,79 @@ def read():
     )  # sort in descending order of created_at timestamp
     return render_template("read.html", docs=docs)  # render the read template
 
+@app.route("/acknowledgement")
+def acknowledgement():
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged transaction
+    """
+    return render_template("acknowledgement.html")
 
-@app.route("/create")
-def create():
+@app.route("/successfullyupdated")
+def successfullyupdated():
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged account creation
+    """
+    return render_template("successfullyupdated.html")
+
+@app.route("/successfullydeleted")
+def successfullydeleted():
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged account creation
+    """
+    return render_template("successfullydeleted.html")
+
+@app.route("/accountack")
+def accountack():
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged account creation
+    """
+    return render_template("accountack.html")
+
+@app.route("/updatepage")
+def updatepage():
+    """
+    Route for GET requests to the read page.
+    Displays some information for the user with links to other pages.
+    """
+    docs = db.exampleapp.find({}).sort(
+        "created_at", -1
+    )  # sort in descending order of created_at timestamp
+    return render_template("updatepage.html", docs=docs)  # render the read template
+
+@app.route("/preexisting")
+def preexisting():
+    """
+    Route for GET requests to the "account number already exists"page.
+    """
+    return render_template("preexisting.html")
+
+@app.route("/nonexistingaccount")
+def nonexistingaccount():
+    """
+    Route for GET request to the error page if there was no object with the entered DBN number.
+    Displays a message and an option to go back and try again
+    """
+    return render_template("nonexistingaccount.html")
+
+@app.route("/invalidreceiver")
+def invalidreceiver():
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged account creation
+    """
+    return render_template("invalidreceiver.html")
+
+@app.route("/nosufficientfunds")
+def nosufficientfunds():
     """
     Route for GET requests to the create page.
     Displays a form users can fill out to create a new document.
     """
-    return render_template("create.html")  # render the create template
-
-
-@app.route("/create", methods=["POST"])
-def create_post():
-    """
-    Route for POST requests to the create page.
-    Accepts the form submission data for a new document and saves the document to the database.
-    """
-    name = request.form["fname"]
-    message = request.form["fmessage"]
-
-    # create a new document with the data the user entered
-    doc = {"name": name, "message": message, "created_at": datetime.datetime.utcnow()}
-    db.exampleapp.insert_one(doc)  # insert a new document
-
-    return redirect(
-        url_for("read")
-    )  # tell the browser to make a request for the /read route
-
+    return render_template("nosufficientfunds.html")  # render the no_sufficient_funds template
 
 @app.route("/edit/<mongoid>")
 def edit(mongoid):
@@ -126,7 +182,120 @@ def edit(mongoid):
         "edit.html", mongoid=mongoid, doc=doc
     )  # render the edit template
 
+@app.route("/initiate/<mongoid>")
+def initiate(mongoid):
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged account creation
+    """
+    docs = db.useraccount.find_one({"_id": ObjectId(mongoid)})
+    return render_template("initiate.html", docs=docs)
 
+@app.route("/successful/<mongoid>")
+def successful(mongoid):
+    """
+    Route for GET request to the acknowledgement page.
+    Display some information about the acknowledged account creation
+    """
+    docs = db.useraccount.find_one({"_id": ObjectId(mongoid)})
+    return render_template("successful.html", docs=docs)
+
+@app.route("/newaccount", methods=["POST"])
+def newaccount_post():
+    id = request.form["fnumber"]
+    if db.useraccount.find({"dbnnumber": {"$eq": id}}).sort("created_at", -1).count() == 0:
+        name = request.form["fname"]
+        age = request.form["fage"]
+        address = request.form["faddress"]
+        email = request.form["femail"]
+        dbnnumber = request.form["fnumber"]
+        amount = request.form["famount"]
+        bname = request.form["fbank"]
+        accnumber= request.form["faccnumber"]
+        message = request.form["fmessage"]
+
+        # create a new document with the data the user entered
+        if bname != "" and accnumber != "" and amount == "":
+            doc1= {"dbnnumber":dbnnumber, "amount": amount, "message": message}
+            db.trasactiondetail.insert_one(doc1)  #insert a new document
+               
+        doc = { "name": name, "age": age, "address": address, "email": email, "dbnnumber": dbnnumber,"amount": amount, "created_at": datetime.datetime.utcnow()}
+        db.useraccount.insert_one(doc)  # insert a new document
+        return redirect(url_for("accountack"))
+    else:
+       return redirect(url_for('preexisting'))
+   
+@app.route("/initiate/<mongoid>", methods=["POST"])
+def initiate_post(mongoid):
+    dbnnumber=db.useraccount.find_one({"_id":{"$eq": ObjectId(mongoid)}})["dbnnumber"]
+    amount= request.form["famount"]
+    message= request.form["fmessage"]
+    
+    doc= {"dbnnumber":dbnnumber, "amount": amount, "message": message}
+    db.trasactiondetail.insert_one(doc)  #insert a new document
+    
+    testsender = db.useraccount.find_one({"dbnnumber":{"$eq": dbnnumber}},{"amount":1})["amount"]
+    b= int(testsender) + int(amount)
+    db.useraccount.update({"dbnnumber":{"$eq": dbnnumber}}, {"$set":{"amount": str(b)}})
+    return redirect(url_for('successful', mongoid=mongoid))
+    
+@app.route("/create", methods=["POST"])
+def create_post():
+    """
+    Route for POST requests to the create page.
+    Accepts the form submission data for a new document and saves the document to the database.
+    """
+    id = request.form["fnumber"]
+    if db.useraccount.find({"dbnnumber": {"$eq": id}}).count() == 1:
+        a = request.form["famount"]
+        if db.useraccount.find({"dbnnumber":{"$eq": id},"amount": {"$gte": a}}).count() == 1:
+            aa = request.form["raccnumber"]
+            if db.useraccount.find({"dbnnumber": {"$eq": aa}}).count() == 1:
+                testsender = db.useraccount.find_one({"dbnnumber":{"$eq": id}},{"amount":1})["amount"]
+                testreceiver= db.useraccount.find_one({"dbnnumber":{"$eq": aa}},{"amount":1})["amount"]
+                dbnnumber = request.form["fnumber"]
+                name = request.form["fname"]
+                rname = request.form["rname"]
+                raccnumber = request.form["raccnumber"]
+                amount = request.form["famount"]
+                message = request.form["fmessage"]
+
+                # create a new document with the data the user entered
+                doc = {"dbnnumber": dbnnumber, "name": name, "rname": rname, "raccnumber": raccnumber,"amount": amount, "message": message, "created_at": datetime.datetime.utcnow()}
+                db.exampleapp.insert_one(doc)  # insert a new document
+                b = int(testsender) - int(a)
+                c = int(testreceiver) + int(a)
+                db.useraccount.update({"dbnnumber":{"$eq": id}}, {"$set":{"amount": str(b)}})
+                db.useraccount.update({"dbnnumber":{"$eq": aa}}, {"$set":{"amount": str(c)}})
+    
+                return redirect(
+                url_for("acknowledgement")  # here i have to redirect it to a new page that acknowledges the transaction!
+                )  # tell the browser to make a request for the /read route
+            else:
+                return redirect(url_for("invalidreceiver"))
+        else:
+            return redirect(url_for("nosufficientfunds"))
+    else:
+        return redirect(url_for("nonexistingaccount"))
+
+@app.route("/updatewindow", methods=["POST"])
+def update_record():
+    id = request.form["dbnnumber"]
+    docu = db.exampleapp.find({"dbnnumber": {"$eq": id}}).sort("created_at", -1)
+    if db.exampleapp.find({"dbnnumber": {"$eq": id}}).sort("created_at", -1).count() == 0:
+        return redirect(url_for('nonexistingaccount'))
+    else:
+        return render_template('updatepage.html', docs=docu)
+
+@app.route("/addmoney", methods=["POST"])
+def addmoney_post():
+    id = request.form["fnumber"]
+    if db.useraccount.find({"dbnnumber": {"$eq": id}}).count() == 1:
+        ss = db.useraccount.find_one({"dbnnumber": {"$eq": id}})["_id"]
+        return redirect(url_for('initiate', mongoid= ss))
+    else:
+        return redirect(url_for('nonexistingaccount'))
+    
 @app.route("/edit/<mongoid>", methods=["POST"])
 def edit_post(mongoid):
     """
@@ -136,12 +305,13 @@ def edit_post(mongoid):
     Parameters:
     mongoid (str): The MongoDB ObjectId of the record to be edited.
     """
-    name = request.form["fname"]
+    recname = request.form["rname"]
     message = request.form["fmessage"]
+    
 
     doc = {
         # "_id": ObjectId(mongoid),
-        "name": name,
+        "rname": recname,
         "message": message,
         "created_at": datetime.datetime.utcnow(),
     }
@@ -151,7 +321,7 @@ def edit_post(mongoid):
     )
 
     return redirect(
-        url_for("read")
+        url_for("successfullyupdated")
     )  # tell the browser to make a request for the /read route
 
 
@@ -166,7 +336,7 @@ def delete(mongoid):
     """
     db.exampleapp.delete_one({"_id": ObjectId(mongoid)})
     return redirect(
-        url_for("read")
+        url_for("successfullydeleted")
     )  # tell the web browser to make a request for the /read route.
 
 
